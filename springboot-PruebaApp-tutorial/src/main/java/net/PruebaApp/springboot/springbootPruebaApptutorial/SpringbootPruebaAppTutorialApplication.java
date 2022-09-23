f3adec09f3c3767e40d05d2e5e2832b5d517e964
alberto97tg <48293253+alberto97tg@users.noreply.github.com>
2022-09-23 11:41:32 +0200

    Add files via upload

---
+++ springboot-PruebaApp-tutorial/src/main/java/net/PruebaApp/springboot/springbootPruebaApptutorial/SpringbootPruebaAppTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.PruebaApp.springboot.springbootPruebaApptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPruebaAppTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPruebaAppTutorialApplication.class, args);
+	}
+
+}
